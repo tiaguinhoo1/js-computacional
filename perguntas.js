06f912600768b0f3c0404d32e15e1c5bf1347be1
tiaguinhoo1 <118751370+tiaguinhoo1@users.noreply.github.com>
2024-08-29 17:33:11 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: " O que mais te fascina na Inteligência Artificial?",
+        alternativas: [
+            {
+                texto: "A) Automação de tarefas",
+                afirmacao: [
+                    "Você está interessado nas aplicações práticas da IA e como ela pode facilitar o dia a dia. Para você, o foco é na utilidade e eficiência."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Capacidade de aprendizado",
+                afirmacao: [
+                    "Você adora aprender sobre as tecnologias por trás da IA e suas implicações para o futuro. Gosta de explorar como IA pode transformar diferentes áreas."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual dessas áreas da IA você acha mais interessante?",
+        alternativas: [
+            {
+                texto: "A) Processamento de linguagem natural 'como chatbots'",
+                afirmacao: [
+                    "Você está interessado nas aplicações práticas da IA e como ela pode facilitar o dia a dia. Para você, o foco é na utilidade e eficiência.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B)Robótica",
+                afirmacao: [
+                    "Você se preocupa com os aspectos éticos e sociais da IA, considerando seu impacto no mercado de trabalho e na sociedade. Você está atento às oportunidades e aos riscos."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você acha que a IA afetará o futuro do trabalho?",
+        alternativas: [
+            {
+                texto: "A) Substituirá muitas funções humanas",
+                afirmacao: [
+                    "Você se interessa pelo uso da IA para fins criativos e inovadores. Gosta de pensar em como IA pode colaborar com humanos para expandir a criatividade e resolver problemas complexos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Aumentará a eficiência das tarefas",
+                afirmacao: [
+                    "Você adora aprender sobre as tecnologias por trás da IA e suas implicações para o futuro. Gosta de explorar como IA pode transformar diferentes áreas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O que você acha do uso da IA em veículos autônomos?",
+        alternativas: [
+            {
+                texto: "A) Incrível, uma revolução no transporte",
+                afirmacao: [
+                    "Você adora aprender sobre as tecnologias por trás da IA e suas implicações para o futuro. Gosta de explorar como IA pode transformar diferentes áreas."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B) Necessário, mas ainda deve ser aprimorado",
+                afirmacao: [
+                    "Você está interessado nas aplicações práticas da IA e como ela pode facilitar o dia a dia. Para você, o foco é na utilidade e eficiência."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você aprendeu sobre IA?",
+        alternativas: [
+            {
+                texto: "A) Documentários e mídias sociais",
+                afirmacao: [
+                    "Você adora aprender sobre as tecnologias por trás da IA e suas implicações para o futuro. Gosta de explorar como IA pode transformar diferentes áreas."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B)Conversando com amigos ou colegas",
+                afirmacao: [
+                    "Você se preocupa com os aspectos éticos e sociais da IA, considerando seu impacto no mercado de trabalho e na sociedade. Você está atento às oportunidades e aos riscos."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é sua principal preocupação em relação à IA?",
+        alternativas: [
+            {
+                texto: "A) Privacidade de dados",
+                afirmacao: [
+                    "Você se preocupa com os aspectos éticos e sociais da IA, considerando seu impacto no mercado de trabalho e na sociedade. Você está atento às oportunidades e aos riscos."
+                ],
+            },
+            {
+                texto: "B) Desemprego causado por automação",
+                afirmacao: [
+                    "Você está interessado nas aplicações práticas da IA e como ela pode facilitar o dia a dia. Para você, o foco é na utilidade e eficiência."
+                ],
+            },
+        ]
+    },
+];
